a9fc563e50bcfcb4d3867d63a74cc3799a5b475b
Karen Sofía Alanis de Anda <ksalanisa@gmail.com>
2026-05-02 22:24:51 -0600

    agregue getters y setters

--- mi_clase.py
+++ mi_clase.py
@@ -1,5 +1,27 @@
 Class Mi_Clase:
-def__init__(self, num1, num2, num3):
-self.__num1 = num1
-self.__num2 = num2
-self.__num3 = num3
+
+    def __init__(self, num1, num2, num3):
+        self.__num1 = num1
+        self.__num2 = num2
+        self.__num3 = num3
+
+    def get_num1(self):
+        return self.__num1
+
+    def get_num2(self):
+        return self.__num2
+
+    def get_num3(self):
+        return self.__num3
+
+    def set_num1(self, num1):
+        self.__num1 = num1
+
+    def set_num2(self, num2):
+        self.__num2 = num2
+
+    def set_num3(self, num3):
+        self.__num3 = num3
+
+
+
